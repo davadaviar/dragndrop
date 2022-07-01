3538257161d0ee771cd237a7cd9de2f31a531adc
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2022-07-01 22:26:45 +0700

    Adding drag event

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,14 @@
 const fill = document.querySelector('.filled');
 const empties = document.querySelectorAll('.empty');
 
+// for loop empties
+for (const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragleave);
+    empty.addEventListener('drop', drop);
+}
+
 // eventlistener
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
