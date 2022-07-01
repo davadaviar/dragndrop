3becc7b2132d71a43e8664ea537c189ccf4fe2ad
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2022-07-01 22:20:36 +0700

    Adding drag dom manipulation

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,16 @@
-const fill = document.querySelector('.')
+const fill = document.querySelector('.filled');
+const empties = document.querySelectorAll('.empty');
+
+// eventlistener
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// drag functions
+function dragStart() {
+   this.className += ' hold';
+   setTimeout(() => this.className = 'invisible', 0);
+}
+
+function dragEnd() {
+    this.className = 'filled'
+}
